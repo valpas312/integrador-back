344832366ced05283673ef5768123257ec4d1fbc
valpas312 <valenpalmas2005@gmail.com>
2023-08-26 21:50:53 -0300

    optimizacion de peticiones

--- routes/turnos.ts
+++ routes/turnos.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getTurnos, verifyTurno, createTurno, softDeleteTurno, hardDeleteTurno, getTurnosAsistidos, getTurnosCancelados, getTurnosConfirmados, getTurnosPendientes } from "../controllers/turnos";
+import { getTurnos, verifyTurno, createTurno, softDeleteTurno, hardDeleteTurno} from "../controllers/turnos";
 import validarJWT from "../middlewares/validarJWT";
 import { recolectarErrores } from "../middlewares/recolectarErrores";
 import { isVerified } from "../middlewares/validarVerificado";
@@ -8,14 +8,6 @@ import { check } from "express-validator";
 const router = Router();
 
 router.get("/", [validarJWT, recolectarErrores], getTurnos);
-
-router.get("/pendientes", [validarJWT, recolectarErrores], getTurnosPendientes);
-
-router.get("/confirmados", [validarJWT, recolectarErrores], getTurnosConfirmados);
-
-router.get("/cancelados", [validarJWT, recolectarErrores], getTurnosCancelados);
-
-router.get("/asistidos", [validarJWT, recolectarErrores], getTurnosAsistidos);
 
 router.post("/", [validarJWT, isVerified,
     check("especialidad", "La especialidad es obligatoria").not().isEmpty(),
