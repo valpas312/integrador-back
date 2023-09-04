7dccd1b4ea94691a6893a5cae85364693b822e47
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-09-03 23:37:03 -0300

    expiracion del token

--- helpers/generarJWT.ts
+++ helpers/generarJWT.ts
@@ -4,7 +4,9 @@ export const generarJWT = (_id: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         const payload = { _id };
 
-        jwt.sign(payload, "clavesecreta", (err, token) => {
+        jwt.sign(payload, "clavesecreta", {
+            expiresIn: '365d'
+        }, (err, token) => {
             if (err) {
                 console.log(err);
                 reject('No se pudo generar el JWT');
